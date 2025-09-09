c6a71ecf4e7cb14589006865d41c5d7f9cabe5fe
윤건 <yungun200512@gmail.com>
2025-09-09 16:07:26 +0900

    회원 가입 최종 수정

--- src/main/java/kopo/fitmate/controller/user/JoinController.java
+++ src/main/java/kopo/fitmate/controller/user/JoinController.java
@@ -1,8 +1,8 @@
 package kopo.fitmate.controller.user;
 
 import jakarta.validation.Valid;
-import kopo.fitmate.dto.JoinDTO;
-import kopo.fitmate.service.IJoinService;
+import kopo.fitmate.dto.user.JoinDTO;
+import kopo.fitmate.service.IUserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -11,51 +11,59 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMapping; // RequestMapping 임포트
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Slf4j
-@RequiredArgsConstructor
 @Controller
-@RequestMapping("/user")
+@RequestMapping("/user") // "/user" 경로를 이 컨트롤러가 담당하도록 명시
+@RequiredArgsConstructor
 public class JoinController {
 
-    private final IJoinService userService;
+    private final IUserService userService;
 
-    // 회원가입 페이지 이동
-    @GetMapping("/userRegForm")
-    public String userRegForm(Model model) {
-        log.info(this.getClass().getName() + ".userRegForm !");
-        // DTO 객체를 모델에 추가하여 Thymeleaf에서 사용
-        model.addAttribute("userDTO", new JoinDTO());
-        return "user/userRegForm"; // templates/user/userRegForm.html
+    /**
+     * 회원가입 폼 페이지로 이동하는 메서드
+     * [GET] /user/joinForm
+     */
+    @GetMapping("/joinForm")
+    public String joinForm(Model model) {
+        log.info(this.getClass().getName() + ".joinForm Start!");
+        model.addAttribute("joinDTO", new JoinDTO());
+        return "user/joinForm"; // "templates/user/joinForm.html"을 찾아 렌더링
     }
 
-    // 회원가입 로직 처리
-    @PostMapping("/insertUserInfo")
-    public String insertUserInfo(@Valid @ModelAttribute JoinDTO pDTO, BindingResult bindingResult, Model model) {
-        log.info(this.getClass().getName() + ".insertUserInfo Start!");
+    /**
+     * Thymeleaf 폼으로부터 회원가입 데이터를 받아 처리하는 메서드
+     * [POST] /user/join
+     */
+    @PostMapping("/join")
+    public String doJoin(@Valid @ModelAttribute("joinDTO") JoinDTO pDTO,
+                         BindingResult bindingResult,
+                         RedirectAttributes redirectAttributes) {
+
+        log.info(this.getClass().getName() + ".doJoin Start!");
 
         if (bindingResult.hasErrors()) {
-            // 유효성 검사 에러가 있으면, 다시 폼으로 돌려보냄
-            return "user/userRegForm";
+            log.warn("Form data validation failed");
+            return "user/joinForm"; // 유효성 검증 실패 시, 다시 회원가입 폼으로
         }
 
         try {
             userService.insertUserInfo(pDTO);
-
-        } catch (RuntimeException e) {
-            log.warn(e.getMessage());
-            // 서비스에서 발생한 예외(중복, 비밀번호 불일치) 처리
-            model.addAttribute("errorMessage", e.getMessage());
-            return "user/userRegForm";
-
+        } catch (IllegalArgumentException e) {
+            log.warn("Join process business error: " + e.getMessage());
+            bindingResult.reject("join.fail", e.getMessage());
+            return "user/joinForm";
         } catch (Exception e) {
-            log.error("회원가입 처리 중 오류 발생", e);
-            model.addAttribute("errorMessage", "처리 중 오류가 발생했습니다.");
-            return "user/userRegForm";
+            log.error("Join process server error", e);
+            bindingResult.reject("join.fail", "시스템 오류가 발생했습니다.");
+            return "user/joinForm";
         }
 
-        // 회원가입 성공 시, 로그인 페이지로 리다이렉트
-        return "redirect:/user/loginForm";
+        log.info(this.getClass().getName() + ".doJoin End!");
+        redirectAttributes.addFlashAttribute("successMsg", "회원가입이 완료되었습니다. 로그인해주세요.");
+        return "redirect:/user/loginForm"; // 로그인 폼 페이지로 리다이렉트
     }
 }
+
--- src/main/java/kopo/fitmate/dto/user/JoinDTO.java
+++ src/main/java/kopo/fitmate/dto/user/JoinDTO.java
@@ -1,4 +1,4 @@
-package kopo.fitmate.dto;
+package kopo.fitmate.dto.user;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
