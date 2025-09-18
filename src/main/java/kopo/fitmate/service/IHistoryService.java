d09e17a29d7ffe07f962bf3a89f15e60a36ec12e
윤건 <yungun200512@gmail.com>
2025-09-18 09:52:22 +0900

    상세보기와 삭제를 위한 로직 추가

--- src/main/java/kopo/fitmate/service/IHistoryService.java
+++ src/main/java/kopo/fitmate/service/IHistoryService.java
@@ -3,18 +3,27 @@ package kopo.fitmate.service;
 import kopo.fitmate.dto.history.AiReportHistoryDTO;
 import kopo.fitmate.dto.history.DietHistoryDTO;
 import kopo.fitmate.dto.history.ExerciseHistoryDTO;
+import kopo.fitmate.repository.mongo.entity.AiReportEntity;
+import kopo.fitmate.repository.mongo.entity.DietInfoEntity;
+import kopo.fitmate.repository.mongo.entity.ExerciseInfoEntity;
 
 import java.util.List;
 
 public interface IHistoryService {
 
-    // 저장된 운동 기록 가져오기
-    List<ExerciseHistoryDTO> getExerciseHistory(Long userNo);
+    // 목록 조회
+    List<ExerciseHistoryDTO> getExerciseHistory(Long userNo);   // 저장된 운동 기록 가져오기
+    List<DietHistoryDTO> getDietHistory(Long userNo);           // 저장된 식단 기록 가져오기
+    List<AiReportHistoryDTO> getAiReportHistory(Long userNo);   // 저장된 AI 리포트 가져오기
 
-    // 저장된 식단 기록 가져오기
-    List<DietHistoryDTO> getDietHistory(Long userNo);
+    // 상세 조회
+    ExerciseInfoEntity getExerciseDetail(String id);
+    DietInfoEntity getDietDetail(String id);
+    AiReportEntity getAiReportDetail(String id);
 
-    // 저장된 AI 리포트 가져오기
-    List<AiReportHistoryDTO> getAiReportHistory(Long userNo);
+    // --- [추가] 삭제 ---
+    void deleteExerciseHistory(String id);
+    void deleteDietHistory(String id);
+    void deleteAiReportHistory(String id);
 
 }
--- src/main/java/kopo/fitmate/service/impl/HistoryService.java
+++ src/main/java/kopo/fitmate/service/impl/HistoryService.java
@@ -6,12 +6,16 @@ import kopo.fitmate.dto.history.ExerciseHistoryDTO;
 import kopo.fitmate.repository.mongo.AiReportRepository;
 import kopo.fitmate.repository.mongo.DietInfoRepository;
 import kopo.fitmate.repository.mongo.ExerciseInfoRepository;
+import kopo.fitmate.repository.mongo.entity.AiReportEntity;
+import kopo.fitmate.repository.mongo.entity.DietInfoEntity;
+import kopo.fitmate.repository.mongo.entity.ExerciseInfoEntity;
 import kopo.fitmate.service.IHistoryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -50,5 +54,47 @@ public class HistoryService implements IHistoryService {
                 .stream()
                 .map(AiReportHistoryDTO::from)
                 .collect(Collectors.toList());
+    }
+
+    // --- [추가] 상세 조회 구현 ---
+    @Override
+    public ExerciseInfoEntity getExerciseDetail(String id) {
+        log.info(this.getClass().getName() + ".getExerciseDetail Start!");
+        // ID로 데이터를 찾고, 없으면 null 반환 (또는 예외 처리)
+        Optional<ExerciseInfoEntity> entity = exerciseInfoRepository.findById(id);
+        return entity.orElse(null);
+    }
+
+    @Override
+    public DietInfoEntity getDietDetail(String id) {
+        log.info(this.getClass().getName() + ".getDietDetail Start!");
+        Optional<DietInfoEntity> entity = dietInfoRepository.findById(id);
+        return entity.orElse(null);
+    }
+
+    @Override
+    public AiReportEntity getAiReportDetail(String id) {
+        log.info(this.getClass().getName() + ".getAiReportDetail Start!");
+        Optional<AiReportEntity> entity = aiReportRepository.findById(id);
+        return entity.orElse(null);
+    }
+
+    // --- [추가] 삭제 구현 ---
+    @Override
+    public void deleteExerciseHistory(String id) {
+        log.info(this.getClass().getName() + ".deleteExerciseHistory Start!");
+        exerciseInfoRepository.deleteById(id);
+    }
+
+    @Override
+    public void deleteDietHistory(String id) {
+        log.info(this.getClass().getName() + ".deleteDietHistory Start!");
+        dietInfoRepository.deleteById(id);
+    }
+
+    @Override
+    public void deleteAiReportHistory(String id) {
+        log.info(this.getClass().getName() + ".deleteAiReportHistory Start!");
+        aiReportRepository.deleteById(id);
     }
 }
