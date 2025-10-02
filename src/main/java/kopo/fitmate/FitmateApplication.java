73cea866c7f868fb3bdb79ee580723512e35a733
윤건 <yungun200512@gmail.com>
2025-10-02 12:56:39 +0900

    RDBMS, NOSQL 구역 나누기

--- src/main/java/kopo/fitmate/FitmateApplication.java
+++ src/main/java/kopo/fitmate/FitmateApplication.java
@@ -2,10 +2,14 @@ package kopo.fitmate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableRedisHttpSession
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "kopo.fitmate.user.repository")
+@EnableMongoRepositories(basePackages = {"kopo.fitmate.exercise.repository", "kopo.fitmate.history.repository"})
 public class FitmateApplication {
 
     public static void main(String[] args) {
