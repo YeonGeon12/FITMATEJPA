063d29e025d22838a3508ab266e54f791a30dc27
yungu <yungun200512@gmail.com>
2025-10-13 00:12:01 +0900

    저장한 운동 루틴, 식단을 볼 수 있게 가져오는 로직 추가

--- src/main/java/kopo/fitmate/diet/repository/DietRepository.java
+++ src/main/java/kopo/fitmate/diet/repository/DietRepository.java
@@ -4,10 +4,15 @@ import kopo.fitmate.diet.repository.entity.DietInfoEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * DietInfoEntity를 MongoDB에서 저장, 조회, 수정, 삭제(CRUD)할 수 있도록 도와주는 인터페이스
  */
 @Repository
 public interface DietRepository extends MongoRepository<DietInfoEntity, String> {
+
+    // 사용자 ID를 기준으로 모든 운동 기록을 'regDt' 필드의 내림차순(최신순)으로 조회
+    List<DietInfoEntity> findByUserIdOrderByRegDtDesc(String userId);
 
 }
--- src/main/java/kopo/fitmate/exercise/repository/ExerciseRepository.java
+++ src/main/java/kopo/fitmate/exercise/repository/ExerciseRepository.java
@@ -4,10 +4,15 @@ import kopo.fitmate.exercise.repository.entity.ExerciseInfoEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * MongoDB의 'EXERCISE_INFO' 컬렉션에 접근하기 위한 Repository 인터페이스
  */
 @Repository
 public interface ExerciseRepository extends MongoRepository<ExerciseInfoEntity, String> {
+
+    // 사용자 ID를 기준으로 모든 운동 기록을 'regDt' 필드의 내림차순(최신순)으로 조회
+    List<ExerciseInfoEntity> findByUserIdOrderByRegDtDesc(String userId);
 
 }
