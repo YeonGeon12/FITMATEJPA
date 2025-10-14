e43696fb259651e962618d74fd0c936f142be34c
yungu <yungun200512@gmail.com>
2025-10-14 23:25:48 +0900

    MyPageController에 있던 비밀번호 찾기 및 재설정 로직 모두 UserController로 이동

--- src/main/java/kopo/fitmate/user/controller/MyPageController.java
+++ src/main/java/kopo/fitmate/user/controller/MyPageController.java
@@ -19,9 +19,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequiredArgsConstructor
 public class MyPageController {
 
-    // [수정] 토큰 유효성 검사를 위해 IUserService 의존성 주입
-    private final IUserService userService;
-
     /**
      * 마이페이지 화면으로 이동
      */
@@ -47,35 +44,5 @@ public class MyPageController {
         model.addAttribute("changePasswordDTO", new ChangePasswordDTO());
         log.info(this.getClass().getName() + ".비밀번호 변경 끝!");
         return "user/changePasswordForm";
-    }
-
-    /**
-     * 비밀번호 찾기(이메일 입력) 폼 페이지로 이동
-     */
-    @GetMapping("/forgotPassword")
-    public String forgotPasswordForm() {
-        log.info(this.getClass().getName() + ".비밀번호 찾기 시작!");
-        return "user/forgotPasswordForm"; // templates/user/forgotPasswordForm.html
-    }
-
-    /**
-     * 이메일 링크를 통해 새 비밀번호 설정 폼 페이지로 이동
-     */
-    @GetMapping("/resetPassword")
-    public String resetPasswordForm(@RequestParam("token") String token, Model model) {
-        log.info(this.getClass().getName() + ".비밀번호 재설정 시작!");
-
-        // 1. 토큰이 유효한지 서비스 레이어에서 검증
-        if (!userService.validatePasswordResetToken(token)) {
-            log.warn("유효하지 않거나 만료된 토큰으로 접근 시도: {}", token);
-            model.addAttribute("errorMsg", "링크가 만료되었거나 유효하지 않습니다. 다시 요청해주세요.");
-            return "user/forgotPasswordForm"; // 유효하지 않으면 다시 이메일 입력 페이지로 보냄
-        }
-
-        // 2. 토큰이 유효하면, 모델에 토큰을 담아 재설정 페이지로 이동
-        model.addAttribute("token", token);
-
-        log.info(this.getClass().getName() + ".비밀번호 재설정 끝!");
-        return "user/resetPasswordForm"; // templates/user/resetPasswordForm.html
     }
 }
--- src/main/java/kopo/fitmate/user/controller/UserController.java
+++ src/main/java/kopo/fitmate/user/controller/UserController.java
@@ -1,16 +1,21 @@
 package kopo.fitmate.user.controller;
 
+import kopo.fitmate.user.service.IUserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
 @Controller
 @RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
+
+    private final IUserService userService;
 
     /**
      * 비밀번호 찾기(이메일 입력) 폼 페이지로 이동
@@ -21,5 +26,24 @@ public class UserController {
         return "user/forgotPasswordForm"; // templates/user/forgotPasswordForm.html
     }
 
-    // 향후 비밀번호 재설정 처리 등 다른 사용자 관련 기능 추가 가능
+    /**
+     * 이메일 링크를 통해 새 비밀번호 설정 폼 페이지로 이동
+     */
+    @GetMapping("/resetPassword")
+    public String resetPasswordForm(@RequestParam("token") String token, Model model) {
+        log.info(this.getClass().getName() + ".비밀번호 재설정 시작!");
+
+        // 1. 토큰이 유효한지 서비스 레이어에서 검증
+        if (!userService.validatePasswordResetToken(token)) {
+            log.warn("유효하지 않거나 만료된 토큰으로 접근 시도: {}", token);
+            model.addAttribute("errorMsg", "링크가 만료되었거나 유효하지 않습니다. 다시 요청해주세요.");
+            return "user/forgotPasswordForm"; // 유효하지 않으면 다시 이메일 입력 페이지로 보냄
+        }
+
+        // 2. 토큰이 유효하면, 모델에 토큰을 담아 재설정 페이지로 이동
+        model.addAttribute("token", token);
+
+        log.info(this.getClass().getName() + ".비밀번호 재설정 끝!");
+        return "user/resetPasswordForm"; // templates/user/resetPasswordForm.html
+    }
 }
