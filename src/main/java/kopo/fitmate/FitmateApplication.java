0e70413e01a27638adaf13ff40d5a77e652d5dfb
yungu <yungun200512@gmail.com>
2025-10-15 16:03:07 +0900

    OpenFegin을 적용하기 위한 메인 함수 설정

--- src/main/java/kopo/fitmate/FitmateApplication.java
+++ src/main/java/kopo/fitmate/FitmateApplication.java
@@ -2,10 +2,12 @@ package kopo.fitmate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableRedisHttpSession
+@EnableFeignClients
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "kopo.fitmate.user.repository")
 public class FitmateApplication {
